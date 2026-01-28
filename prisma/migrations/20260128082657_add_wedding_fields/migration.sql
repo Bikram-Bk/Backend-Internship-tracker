b313fea07137bcb5402d114d4b7d33d076ed3148
Bikram_Luhar <bikrambk2244@gmail.com>
2026-01-29 00:12:07 +0545

    Prism Schema Updated

---
+++ prisma/migrations/20260128082657_add_wedding_fields/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "events" ADD COLUMN     "brideName" TEXT,
+ADD COLUMN     "groomName" TEXT;
---
+++ prisma/migrations/20260128145622_add_pasni_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "events" ADD COLUMN     "babyDob" TIMESTAMP(3),
+ADD COLUMN     "babyGender" TEXT,
+ADD COLUMN     "babyName" TEXT,
+ADD COLUMN     "muhurtaTime" TEXT;
